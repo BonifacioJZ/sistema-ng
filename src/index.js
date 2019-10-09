6afd883e54705944e948af8bd88a19456161d068
Bonifacio Juarez Ceja <revanjz6@gmail.com>
2019-10-09 18:05:26 -0500

    Configuracion de graphql para poder realizar un login y la integracion de apollo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import ApolloClient  from 'apollo-boost';
+import {ApolloProvider} from '@apollo/react-hooks';
 import App from './global/App';
 import 'antd/dist/antd.css';
+const client = new ApolloClient({
+    uri:"http://localhost:8000/graphql/"
+})
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider>, document.getElementById('root'));
 
